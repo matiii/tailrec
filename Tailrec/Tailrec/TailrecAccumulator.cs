315d06068672a048a6c917d150b86a8390e0340e
Mateusz Mazurek <mateusz.mazurek@gmail.com>
2017-08-04 12:49:15 +0200

    shell of library

--- Tailrec/Tailrec/TailrecAccumulator.cs
+++ Tailrec/Tailrec/TailrecAccumulator.cs
@@ -1,8 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace Tailrec
 {
-    public class TailrecAccumulator
+    public class TailrecAccumulator<T1, T2, TResult>
     {
+        private struct ParametersInput
+        {
+            public T1 T1 { get; set; }
+            public T2 T2 { get; set; }
+        }
+
+        private readonly List<ParametersInput> _accumulated = new List<ParametersInput>(5);
+
+        public TailrecAccumulator(MethodInfo methodInfo, object target)
+        {
+        }
+
+        public TResult Accumulator(T1 t1, T2 t2)
+        {
+            
+        }
     }
 }
