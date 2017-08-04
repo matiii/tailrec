e5bf165d5db102b191f24a57d68a7793499a7bc2
Mateusz Mazurek <mateusz.mazurek@gmail.com>
2017-08-04 12:02:01 +0200

    implement recurent demo

---
+++ Tailrec/Tailrec.Demo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Tailrec.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var program = new Program();
+
+            int result = program.Method(0, 10);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        public int Method(int x, int y)
+        {
+            if (y > 0)
+            {
+                return Method(x + 1, y - 1);
+            }
+
+            return x;
+        }
+    }
+}
