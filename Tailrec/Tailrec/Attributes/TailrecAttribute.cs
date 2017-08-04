8490b89dcf63319aacabf9571110a1b8c0dca230
Mateusz Mazurek <mateusz.mazurek@gmail.com>
2017-08-04 11:51:51 +0200

    initail

---
+++ Tailrec/Tailrec/Attributes/TailrecAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Tailrec.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class TailrecAttribute: Attribute
+    {
+    }
+}
